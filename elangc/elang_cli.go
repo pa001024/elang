97ecef86621998b1c7f6870e79c10f435b11f909
pa001024 <pa001024@qq.com>
2013-07-24 20:48:09 +0800

    add -v mode

--- elangc/elang_cli.go
+++ elangc/elang_cli.go
@@ -5,14 +5,32 @@ import (
 	"github.com/pa001024/elang"
 	"io"
 	"os"
+
+	"bytes"
+	"fmt"
 )
 
 func main() {
 	isDecode := flag.Bool("d", false, "Decode?")
+	isView := flag.Bool("v", false, "View?")
 	flag.Parse()
 	if *isDecode {
-		io.Copy(os.Stdout, elang.NewDecoder(os.Stdin))
+		if *isView {
+			buf := &bytes.Buffer{}
+			io.Copy(buf, elang.NewDecoder(os.Stdin))
+			fmt.Printf("%#v\n", buf.Bytes())
+			io.Copy(os.Stdout, buf)
+		} else {
+			io.Copy(os.Stdout, elang.NewDecoder(os.Stdin))
+		}
 	} else {
-		io.Copy(elang.NewEncoder(os.Stdout), os.Stdin)
+		if *isView {
+			buf := &bytes.Buffer{}
+			io.Copy(elang.NewEncoder(buf), os.Stdin)
+			fmt.Printf("%#v\n", buf.Bytes())
+			io.Copy(os.Stdout, buf)
+		} else {
+			io.Copy(elang.NewEncoder(os.Stdout), os.Stdin)
+		}
 	}
 }
